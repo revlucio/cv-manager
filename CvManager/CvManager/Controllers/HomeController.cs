7d11adcf11248dfc81655b5bc995381a36e79ec3
revlucio <revlucio@gmail.com>
2014-05-05 21:25:30 +0100

    Adding Liz's CV to the project

--- CvManager/CvManager/Controllers/HomeController.cs
+++ CvManager/CvManager/Controllers/HomeController.cs
@@ -1,14 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace CvManager.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Cv()
         {
             return View();
         }
